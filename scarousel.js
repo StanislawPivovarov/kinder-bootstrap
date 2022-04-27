a45dad92f2a4b43e79ad03155bbf9f13c1977204
Stanislaw Pivovarov <photovarov@bk.ru>
2022-04-27 12:54:25 +0300

    pattern added to 5th block

--- scarousel.js
+++ scarousel.js
@@ -6,4 +6,13 @@ $(document).on('ready', function() {
       prevArrow: '<img src="img/10/leaf-left.png" class="slick-prev slick-arrow" style="width: 5%; height: auto; z-index: 10; left: -90px">',
       nextArrow: '<img src="img/10/leaf-right.png" class="slick-next slick-arrow" style="width: 5%; height: auto; right: -70px";>',
     });
+  });
+
+$(document).on('ready', function() {
+    $('.carousel-2').slick({
+      infinite: true,
+      slidesToShow: 1,
+      prevArrow: '<img src="img/10/leaf-left.png" class="slick-prev slick-arrow" style="width: 12%; height: auto; z-index: 0; left: -40px;">',
+      nextArrow: '<img src="img/10/leaf-right.png" class="slick-next slick-arrow" style="width: 12%; height: auto; z-index:; right: -40px";>',
+    });
   })
